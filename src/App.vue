9192737043394782925a8cd33d03a836b27e3895
6I3L <145340475+6I3L@users.noreply.github.com>
2024-01-17 17:41:58 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <v-main>
+      <HelloWorld />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import * as d3 from "d3"
+
+onMounted(async () => {
+  const rawData = await d3.csv('taylorswift.csv');
+  console.log(rawData);
+});
+  //
+</script>
